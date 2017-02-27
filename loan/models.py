6a456df64302e5f5aa67dd2509b0a55c4870ce56
Fanuel Omondi Otieno <fanuel@aims.ac.za>
2017-02-27 21:46:39 +0200

    Added models to the bank app

--- loan/models.py
+++ loan/models.py
@@ -3,20 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.timezone import now
 
+from bank.models import Addresse
+
 # Create your models here.
-
-class Addresse(models.Model):
-    postal_address = models.TextField(max_length=1000)
-    email = models.EmailField()
-    phone_no = models.IntegerField()
-    tel_no = models.IntegerField(blank=True)
-    resident_town = models.CharField(max_length=20)
-    estate = models.CharField(max_length=20)
-    house_no = models.IntegerField(blank=True)
-
-    def __init__(self):
-        return "Address details for: %s" % self.phone_no
-
 
 ########################################################################################################################
 class Expenses(models.Model):
@@ -129,60 +118,13 @@ class Loan(models.Model):
     child_support_description = models.CharField(max_length=1000, blank=True)
 
 
-########################################################################################################################
-class SavingAccount(models.Model):
-    account_number = models.IntegerField(auto_created=True, editable=False, primary_key=True)
-    amount = models.DecimalField(decimal_places=2, max_digits=7, default=0, blank=True)
-    reg_date = models.DateTimeField(default=now)
+
+
+
 
 
 ########################################################################################################################
-class CurrentAccount(models.Model):
-    account_number = models.IntegerField(auto_created=True, editable=False, primary_key=True)
-    amount = models.DecimalField(decimal_places=2, max_digits=7, default=0, blank=True)
-    reg_date = models.DateTimeField(default=now)
 
-
-########################################################################################################################
-class ATMCard(models.Model):
-    number = models.IntegerField(auto_created=True, editable=False, primary_key=True)
-    expiry_date = models.DateTimeField()
-    csv = models.IntegerField()
-    reg_date = models.DateTimeField(default=now)
-    atm_saving = models.ForeignKey(SavingAccount, blank=True)
-    atm_current = models.ForeignKey(CurrentAccount)
-    reg_date = models.DateTimeField(default=now)
-
-
-########################################################################################################################
-class Personal_Information(models.Model):
-    first_name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    other_names = models.CharField(max_length=50)
-    id_number = models.IntegerField(primary_key=True)
-    passport = models.BooleanField(default=False)
-    passport_no = models.IntegerField(blank=True)
-    pin_no = models.IntegerField()
-    ########################################################################################################################
-    address = models.ForeignKey(Addresse)
-    ########################################################################################################################
-    current_ac = models.ForeignKey(CurrentAccount)
-    ########################################################################################################################
-    economic = models.ForeignKey(Economic)
-    ########################################################################################################################
-    saving_ac = models.ForeignKey(SavingAccount, blank=True)
-    ########################################################################################################################
-    register_atm = models.BooleanField(default=True)
-    atm_card = models.ForeignKey(ATMCard)
-    ########################################################################################################################
-    loan_ac = models.ForeignKey(Loan, blank=True)
-    ########################################################################################################################
-    date_registered = models.DateTimeField(default=now)
-
-    ########################################################################################################################
-
-    def __init__(self):
-        return "%d:%s" % (str(self.id_number), self.surname)
 
     ########################################################################################################################
 
