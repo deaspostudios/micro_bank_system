d6f5a56d7bdeb4b314b4c06259c14a1e0ab0debc
Polycarp Okock <polycarpokock@live.com>
2017-03-09 18:23:08 +0300

    Adding the credit score functions, setting up almost done

--- loan/credit_score.py
+++ loan/credit_score.py
@@ -1,1 +1,118 @@
 # coding=utf-8
+from django.shortcuts import get_object_or_404
+
+from bank.models import Personal_Information
+
+
+########################################################################################################################
+
+# credit scoring system function will be written here
+# Analyzes loan worthiness and limits amount available to be borrowed
+
+########################################################################################################################
+
+class Account(object):
+    def __init__(self, acc_no):
+        self.account_no = acc_no
+        # an account number has to belong to a person
+        self.holder = get_object_or_404(Personal_Information, pk=acc_no)
+        # an account has the following
+        self.current = None
+        self.saving = None
+        self.loan = None
+        self.atm = None
+        self.registrationdate = None
+        self.creditscore = None
+
+    def personal_detail(self, *args, **kwargs):
+        self.registrationdate = self.holder.date_registered
+        return self.registrationdate
+
+    def personloan(self):
+        self.loan = self.holder.loan_ac
+        return self.loan
+
+    def personsaving(self):
+        self.saving = self.holder.saving_ac
+        return self.saving
+
+    def personcurrent(self):
+        self.current = self.holder.current_ac
+        return self.current
+
+
+# Objects inheriting from Class Account
+
+class Current(Account):
+    def __init__(self, acc_no, amount, reg_date):
+        # applies the same mro as the parent
+        super(Current, self).__init__(acc_no)
+        # other items that make up the current account
+        self.amount = amount
+        self.reg_date = reg_date
+        # a current accounts may be registered with an ATM
+        self.atm = None
+
+        # ToAdd functions for evaluating credit score
+
+        def balance(*args, **kwargs):
+            if self.amount < 0:
+                score = -1
+            elif self.amount < self.saving:
+                score = -0.5
+            elif self.amount == self.saving:
+                score = 0
+            elif self.amount > self.saving:
+                score = 0.5
+            return score
+
+        def loanCompbalance(*args, **kwargs):
+            if self.amount < self.loan.loan_amount:
+                score = -1
+            elif self.amount == self.loan.loan_amount:
+                score = 0
+            elif self.amount > self.loan.loan_amount:
+                score = 1
+
+                # To find out, how does having an ATM affect one credibily
+
+                # Tasked Shem to define how active the account has been, suggest this be defined at account level
+
+
+class Saving(Account):
+    def __init__(self, acc_no, amount, reg_date):
+        # applies the same mro as the parent
+        super(Saving, self).__init__(acc_no)
+        # other items that make up the current account
+        self.amount = amount
+        self.reg_date = reg_date
+
+    # The savings has to exist for a person tobe given  a loan
+    def savingexist(self):
+        if self.saving:
+            return 0
+            # Other rules like the minimu amount deposited etc.
+
+            # Tasked Shem to define how active the account has been, suggest this be defined at account level
+
+            # ToAdd functions for evaluating credit score
+
+
+class Atm(Current):
+    def __init__(self, number, expiry, attached):
+        self.atm = attached
+        self.number = number
+        self.expity = expiry
+
+        # ToAdd functions for evaluating credit score
+
+
+class Loan(Account):
+    # this class will super alot fro other class functions+
+    def __init__(self, acc_no, type, amount):
+        super(Loan, self).__init__(acc_no)
+        self.type = type
+        self.amount = amount
+        self.loanee = None
+        self.status = None
+        # def status(self, *args, **kwargs):
